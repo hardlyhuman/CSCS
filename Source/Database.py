8923062a06118379ae073f072a2ddd23b325cfa1
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-12-19 21:09:37 +0530

    Added Systems Table

--- Source/Database.py
+++ Source/Database.py
@@ -1,4 +1,15 @@
 import sqlite3
 
+#   Setting up database connection
 conn = sqlite3.connect('CSCSMaster.db')
 print("Successful connection with database")
+
+#   System model Creation
+conn.execute(("""CREATE TABLE "system" ( 
+                "ID" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+                "IP" char(15) NOT NULL,
+                "OS" VARCHAR(25) NOT NULL,
+                "EntryDate" DATETIME NULL 
+                );"""))
+
+print("Successfully Created System Table")
