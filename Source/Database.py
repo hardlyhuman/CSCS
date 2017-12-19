f4a2de4564d4ccb619d0ad43fa74b18f09aa2d76
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2017-12-19 22:20:42 +0530

    Added User Table

--- Source/Database.py
+++ Source/Database.py
@@ -6,10 +6,21 @@ print("Successful connection with database")
 
 #   System model Creation
 conn.execute(("""CREATE TABLE "system" ( 
-                "ID" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-                "IP" char(15) NOT NULL,
+                "ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                "IP" CHAR(15) NOT NULL,
                 "OS" VARCHAR(25) NOT NULL,
                 "EntryDate" DATETIME NULL 
                 );"""))
 
 print("Successfully Created System Table")
+
+
+conn.execute(("""CREATE TABLE "user" (
+                 "ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                 "NAME" VARCHAR(30) NULL ,
+                 "HOST" VARCHAR(30) NULL ,
+                 "IP" INTEGER NOT NULL
+                 REFERENCES "system" ("ID") 
+                 );"""))
+
+print("Successfully Created User Table")
